4ede3bc19340f1827732c7472efc0d50faac3815
Aamin887 <forkahamin@yahoo.co.uk>
2024-06-10 23:26:23 +0100

    cors resolved

--- config/allowedOrgin.js
+++ config/allowedOrgin.js
@@ -1,9 +1,9 @@
 const allowedOrigin = [
   "http://localhost:3000",
-  "http://localhost:5471",
-  "http://localhost:5472",
-  "http://localhost:5473",
-  "http://localhost:5474",
+  "http://localhost:5171",
+  "http://localhost:5172",
+  "http://localhost:5173",
+  "http://localhost:5174",
   "http://localhost:3001",
   "http://localhost:5001",
 ];
